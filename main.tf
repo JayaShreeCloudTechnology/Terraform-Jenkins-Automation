2507207ae57da7bbad55e2c4f49c77921c6d6a28
JayaShreeCloudTechnology <jayashreecloudtechnology@gmail.com>
2026-08-12 19:51:25 +0530

    added s3 bucket

--- main.tf
+++ main.tf
@@ -1,11 +1,23 @@
 provider "aws" {
-    region = "ap-south-1"  
+  region = "ap-south-1"
 }
 
+# EC2 Instance
 resource "aws_instance" "foo" {
   ami           = "ami-035827357e3c7e810" # ap-south-1
   instance_type = "t3.micro"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
+  }
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "demo" {
+  bucket = "terraform-demo-bucket-2026-12345-hyd"
+
+  tags = {
+    Name        = "Terraform-S3"
+    Environment = "Dev"
   }
 }
