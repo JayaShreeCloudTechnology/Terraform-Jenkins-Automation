b37cc0fabe092dc880face496e26069adaaa216b
JayaShreeCloudTechnology <jayashreecloudtechnology@gmail.com>
2026-09-10 18:01:02 +0530

    updated main.tf file

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
 provider "aws" {
-<<<<<<< HEAD
   region = "ap-south-1"
-=======
-  region = "us-east-1"
->>>>>>> ab6069f (updated the main.tf & test file)
 }
 
 # EC2 Instance
 resource "aws_instance" "foo" {
-<<<<<<< HEAD
   ami           = "ami-01a00762f46d584a1" # ap-south-1
-=======
-  ami           = "ami-0b6d9d3d33ba97d99" # us-east-1
->>>>>>> ab6069f (updated the main.tf & test file)
   instance_type = "t3.micro"
 
   tags = {
@@ -22,17 +14,14 @@ resource "aws_instance" "foo" {
 
 # S3 Bucket
 resource "aws_s3_bucket" "demo" {
-<<<<<<< HEAD
   bucket = "tcs-hyd-test-bucket-2027-100"
-=======
-  bucket = "hydtestbucket20261234"
->>>>>>> ab6069f (updated the main.tf & test file)
 
   tags = {
     Name        = "Terraform-S3"
     Environment = "Dev"
   }
 }
+
 # Create VPC
 resource "aws_vpc" "demo-vpc" {
   cidr_block       = "11.0.0.0/16"
@@ -42,3 +31,5 @@ resource "aws_vpc" "demo-vpc" {
     Name = "test-vpc"
   }
 }
+
+
